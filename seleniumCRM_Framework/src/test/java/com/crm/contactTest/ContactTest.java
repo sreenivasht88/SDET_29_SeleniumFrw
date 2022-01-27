fbf686683443c2c77d828c01288b29e181ae85f0
qspidersseleniumoar <qspidersseleniumoar1234@gmail.com>
2022-01-27 15:34:22 +0530

    my first commit

---
+++ seleniumCRM_Framework/src/test/java/com/crm/contactTest/ContactTest.java
@@ -0,0 +1,17 @@
+package com.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test(groups = "smokeTest")
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+	}
+	
+	@Test(groups = "regressionTest")
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+
+}
---
+++ seleniumCRM_Framework/src/test/java/com/crm/orgTest/OrganizationTest.java
@@ -0,0 +1,21 @@
+package com.crm.orgTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+
+	@Test(groups = "smokeTest")
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest==>"+ System.getProperty("browser"));
+
+	}
+	
+	@Test(groups = "regressionTest")
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
